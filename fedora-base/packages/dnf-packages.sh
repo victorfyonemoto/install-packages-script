877888d3bd7dbc53664ce6eb0a82f3dd800d1a5f
Victor Ferreira Yonemoto <victor.softwaredesign@gmail.com>
2025-01-30 13:43:53 -0300

    Added install packages script for Fedora base systems

---
+++ fedora-base/packages/dnf-packages.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Packages
+dnf_packages=(
+	### System
+	"synaptic"
+	"timeshift"
+	### Utils
+	"gzip"
+	"bat"
+	"fzf"
+	"ripgrep"
+	"fd-find"
+	"vifm"
+	"gnome-tweaks"
+	"piper"
+	### Office
+	#"pdfarranger"
+	### Programming
+	"git"
+	"neovim"
+	### Midia
+	"vlc"
+	"celluloid"
+	"transmission"
+	#"transmission-gtk"
+	### Compatibility
+	"wine"
+	"winetricks"
+)
+
+# Function to install DNF packages
+install_dnf_packages() {
+	echo "Installing packages..."
+	for pkg in "${dnf_packages[@]}"; do
+		if ! rpm -q "$pkg" &>/dev/null; then
+			echo "===== Installing $pkg... ====="
+			dnf install -y "$pkg" || { echo "Failed to install $pkg"; exit 1; }
+		else
+			echo "$pkg is already installed."
+		fi
+	done
+	echo "All specified packages have been installed."
+}
---
+++ fedora-base/packages/snaps.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Snap applications
+snap_apps=(
+	###	Utils
+	"shfmt"
+)
+
+# Function to install Snap applications
+install_snaps() {
+	 # Check if snap is installed
+	if ! command -v snap &> /dev/null; then   # Check if snap is installed
+  	echo "Snapd is not installed. Installing snapd..."
+
+		# Try to install snapd
+		if ! dnf install snapd -y; then
+			echo "Failed to install snapd. Skipping snap apps installation."
+			return
+		fi
+
+		# Ensure snapd is started
+    if ! systemctl enable --now snapd.socket; then
+			echo "Failed to start snapd service. Skipping Snap apps installation."
+			return
+		fi
+	fi
+	
+	# Ensure snapd is active before continuing
+	if ! systemctl is-active --quiet snapd.socket; then  # Ensure snapd service is active
+		echo "Starting snapd.socket..."
+		systemctl enable --now snapd.socket
+	fi
+	
+	echo "Installing Snaps..."
+	for snap in "${snap_apps[@]}"; do
+		echo "===== Installing $snap... ====="
+		snap install "$snap" || { echo "Failed to install $snap"; exit 1; }
+	done
+	echo "All specified Snaps have been installed."
+}
